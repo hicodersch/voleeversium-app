ff1d8d2596ed084d9cba249d46de5af03587a488
hsynsvndk <hsyn.svndk@hotmail.com>
2022-09-17 19:24:44 +0200

    bug fixed

--- src/Pages/Role.jsx
+++ src/Pages/Role.jsx
@@ -1,6 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function TypesExample() {
     const navigate = useNavigate();
@@ -8,10 +10,28 @@ function TypesExample() {
     const navigateToRole = ()=> navigate("/signup");
 
   return (
-    <>
-      <Button variant="success" onClick={navigateToRole}>Volee</Button>{' '}
-      <Button variant="warning" onClick={navigateToRole}>NonPro</Button>{' '}
-    </>
+    <Container fluid  className="mt-5 flex-column d-flex align-items-center justify-content-center">
+        <Row className="justify-content-center">
+        <Col  lg="8">
+        <img
+          alt=""
+          src="/assets/logo.png"
+          width="300"
+          height="200"
+          className="align-top"/>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col  lg="6">
+        <Button  style={{ width: "25rem", height: "35rem" }} variant="success" onClick={navigateToRole}>Volee</Button>{' '}
+        </Col>
+        <Col  lg="6">
+        <Button style={{ width: "25rem", height: "35rem" }}  variant="warning" onClick={navigateToRole}>NonPro</Button>{' '}
+        </Col>
+      </Row>
+      
+      
+    </Container>
   );
 }
 
