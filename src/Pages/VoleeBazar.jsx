d6f1579ef6a6eca74a25cddc0cf2f8a5516fe6f1
hsynsvndk <hsyn.svndk@hotmail.com>
2022-09-17 23:08:10 +0200

    voleebazar

--- src/Pages/VoleeBazar.jsx
+++ src/Pages/VoleeBazar.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
-
+import React from 'react';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import EngageComponent from '../components/EngageComponent';
+import NavbarComponent from '../components/NavbarVoleeComponent';
+import VoleeBon from '../components/VoleeBon';
 function VoleeBazar() {
   return (
-    <div>VoleeBazar</div>
+   <>
+   <NavbarComponent></NavbarComponent>
+   <Container
+      className="mt-5 flex-column d-flex align-items-center justify-content-center"
+    >
+      <Row className="justify-content-center">
+        <Col>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Skills</Form.Label>
+            <Form.Control type="email" placeholder="#English" />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>From</Form.Label>
+            <Form.Control type="email" placeholder="13.10.2022 22:30" />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>To</Form.Label>
+            <Form.Control type="email" placeholder="13.10.2022 22:30" />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Language</Form.Label>
+            <Form.Control type="email" placeholder="German" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+        <Col className="mt-5" lg="4">
+          <VoleeBon></VoleeBon>
+        </Col>
+
+      </Row>
+    </Container>
+   </>
   )
 }
 
