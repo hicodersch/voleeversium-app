c82e75d51841081e14de9d713c5b4c9e150805d5
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2022-09-17 23:08:02 +0200

    provolee

--- src/Pages/ProvoleeList.jsx
+++ src/Pages/ProvoleeList.jsx
@@ -3,190 +3,106 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
+import NavbarComponent from "../components/NavbarNpoComponent";
+import Badge from "react-bootstrap/Badge";
 
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 function ProvoleeList() {
+  const provoleeList = [
+    {
+      projectName: "FullStack Web Development Course",
+      image: "https://teammeta.eu/wp-content/uploads/2020/07/developer.jpg",
+      skills: [
+        { skill: "JS", color: "primary" },
+        { skill: "HTML", color: "warning" },
+        { skill: "NODE", color: "info" },
+        { skill: "React", color: "success" },
+      ],
+      schedule: "4 hours every Saturday",
+    },
+    {
+      projectName: "FullStack Web Development Course",
+      image: "https://teammeta.eu/wp-content/uploads/2020/07/developer.jpg",
+      skills: [
+        { skill: "JS", color: "primary" },
+        { skill: "HTML", color: "warning" },
+        { skill: "NODE", color: "info" },
+        { skill: "React", color: "success" },
+      ],
+      schedule: "4 hours every Saturday",
+    },
+    {
+      projectName: "FullStack Web Development Course",
+      image: "https://teammeta.eu/wp-content/uploads/2020/07/developer.jpg",
+      skills: [
+        { skill: "JS", color: "primary" },
+        { skill: "HTML", color: "warning" },
+        { skill: "NODE", color: "info" },
+        { skill: "React", color: "success" },
+      ],
+      schedule: "4 hours every Saturday",
+    },
+    {
+      projectName: "FullStack Web Development Course",
+      image: "https://teammeta.eu/wp-content/uploads/2020/07/developer.jpg",
+      skills: [
+        { skill: "JS", color: "primary" },
+        { skill: "HTML", color: "warning" },
+        { skill: "NODE", color: "info" },
+        { skill: "React", color: "success" },
+      ],
+      schedule: "4 hours every Saturday",
+    },
+  ];
   return (
-    <Container
-      fluid
-      className="mt-5 flex-column d-flex align-items-center justify-content-center"
-    >
-      <Row className="justify-content-center">
-        <Col className="mt-5" lg="4">
-          <Card style={{ width: "25rem", height: "35rem" }}>
-            <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-              <Card.Img
-                className="rounded-circle"
-                style={{ width: "10rem", height: "10rem" }}
-                variant="top"
-                src="https://hicoders.ch/wp-content/uploads/2019/03/crest.png"
-              />
-              <Card.Title>Project Name</Card.Title>
-              <Card.Text>HiCoders</Card.Text>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel s
-              </Card.Text>
-              <Card.Img
-                style={{ width: "8rem", height: "5rem" }}
-                variant="top"
-                src="/assets/star.png"
-              />
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mt-5" lg="4">
-        <Card style={{ width: "25rem", height: "35rem" }}>
-            <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-              <Card.Img
-                className="rounded-circle"
-                style={{ width: "10rem", height: "10rem" }}
-                variant="top"
-                src="https://hicoders.ch/wp-content/uploads/2019/03/crest.png"
-              />
-              <Card.Title>Project Name</Card.Title>
-              <Card.Text>HiCoders</Card.Text>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel s
-              </Card.Text>
-              <Card.Img
-                style={{ width: "8rem", height: "5rem" }}
-                variant="top"
-                src="/assets/star.png"
-              />
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mt-5" lg="4">
-          <Card style={{ width: "25rem", height: "35rem" }}>
-            <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-              <Card.Img
-                className="rounded-circle"
-                style={{ width: "10rem", height: "10rem" }}
-                variant="top"
-                src="https://i.pinimg.com/originals/44/9e/5e/449e5e93c14f34fb7b90559e4f9908ab.jpg"
-              />
-              <Card.Title>User Name</Card.Title>
-              <Card.Text>Senior Developer</Card.Text>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel s
-              </Card.Text>
-              <Card.Img
-                style={{ width: "8rem", height: "5rem" }}
-                variant="top"
-                src="/assets/star.png"
-              />
-              <Form>
-                <Container
-                  fluid
-                  className="mt-5 flex-column d-flex align-items-center justify-content-center"
-                >
-                  <Row className="justify-content-center">
-                    <Col lg="6">
-                      <Button variant="success text-white" type="submit">
-                        Match
-                      </Button>
-                    </Col>
-                    <Col lg="6">
-                      <Button variant="warning text-white" type="submit">
-                        Pass
-                      </Button>
-                    </Col>
-                  </Row>
-                </Container>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mt-5" lg="4">
-          <Card style={{ width: "25rem", height: "35rem" }}>
-            <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-              <Card.Img
-                className="rounded-circle"
-                style={{ width: "10rem", height: "10rem" }}
-                variant="top"
-                src="https://i.pinimg.com/originals/44/9e/5e/449e5e93c14f34fb7b90559e4f9908ab.jpg"
-              />
-              <Card.Title>User Name</Card.Title>
-              <Card.Text>Senior Developer</Card.Text>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel s
-              </Card.Text>
-              <Card.Img
-                style={{ width: "8rem", height: "5rem" }}
-                variant="top"
-                src="/assets/star.png"
-              />
-              <Form>
-                <Container
-                  fluid
-                  className="mt-5 flex-column d-flex align-items-center justify-content-center"
-                >
-                  <Row className="justify-content-center">
-                    <Col lg="6">
-                      <Button variant="success text-white" type="submit">
-                        Match
-                      </Button>
-                    </Col>
-                    <Col lg="6">
-                      <Button variant="warning text-white" type="submit">
-                        Pass
-                      </Button>
-                    </Col>
-                  </Row>
-                </Container>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mt-5" lg="4">
-          <Card style={{ width: "25rem", height: "35rem" }}>
-            <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-              <Card.Img
-                className="rounded-circle"
-                style={{ width: "10rem", height: "10rem" }}
-                variant="top"
-                src="https://i.pinimg.com/originals/44/9e/5e/449e5e93c14f34fb7b90559e4f9908ab.jpg"
-              />
-              <Card.Title>User Name</Card.Title>
-              <Card.Text>Senior Developer</Card.Text>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel s
-              </Card.Text>
-              <Card.Img
-                style={{ width: "8rem", height: "5rem" }}
-                variant="top"
-                src="/assets/star.png"
-              />
-              <Form>
-                <Container
-                  fluid
-                  className="mt-5 flex-column d-flex align-items-center justify-content-center"
-                >
-                  <Row className="justify-content-center">
-                    <Col lg="6">
-                      <Button variant="success text-white" type="submit">
-                        Match
-                      </Button>
-                    </Col>
-                    <Col lg="6">
-                      <Button variant="warning text-white" type="submit">
-                        Pass
-                      </Button>
-                    </Col>
-                  </Row>
-                </Container>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <NavbarComponent></NavbarComponent>
+
+      <Container className="mt-5 flex-column d-flex align-items-center justify-content-center">
+        <Row className="justify-content-center">
+          {provoleeList.map((pro) => {
+            return (
+              <Col className="mt-5" lg="4">
+                <Card style={{ width: "25rem", height: "35rem" }}>
+                  <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                    <Card.Img
+                      className="rounded-circle"
+                      style={{ width: "10rem", height: "10rem" }}
+                      variant="top"
+                      src="https://teammeta.eu/wp-content/uploads/2020/07/developer.jpg"
+                    />
+                    <Card.Title>Project Name</Card.Title>
+                    <Card.Text>{pro.projectName}</Card.Text>
+                    <Card.Title>Requirements</Card.Title>
+                    <Card.Text>
+                      <strong>Skills</strong>
+                    </Card.Text>
+
+                    <Card.Text>
+                      <Row className="bg-light m-3">
+                        {pro.skills.map((skill) => {
+                          return (
+                            <Col lg="2">
+                              <Badge bg={skill.color}>{skill.skill}</Badge>{" "}
+                            </Col>
+                          );
+                        })}
+                      </Row>
+                    </Card.Text>
+                    <Card.Text>
+                      <strong>Schedule</strong>{" "}
+                    </Card.Text>
+                    <Card.Text>{pro.schedule}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+
+        </Row>
+      </Container>
+    </>
   );
 }
 
